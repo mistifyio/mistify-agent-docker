7f188e1197d0b1678869a9cf3ed4a7b33a753035
Brian Bickerton <bbickerton@omniti.com>
2015-03-20 14:46:59 +0000

    Test with 'warn' log level

--- mdocker_test.go
+++ mdocker_test.go
@@ -25,7 +25,7 @@ var client TestClient
 // TestMain sets up the server and RPC client before running tests
 func TestMain(m *testing.M) {
 	var port uint = 30001
-	logLevel := "info"
+	logLevel := "warn"
 	if err := logx.DefaultSetup(logLevel); err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
