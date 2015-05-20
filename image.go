b89448c06dc4beb55a769570036d97a772665fd1
Brian Bickerton <bbickerton@omniti.com>
2015-05-20 14:48:27 +0000

    Change ImportImage to LoadImage to allow rename and preserve metadata
    Due to issues with metadata preservation and repo renaming (tagging)
    using either docker.ImportImage or docker.LoadImage directly, neither
    were acceptable. Instead, handle the rename by updating the repositories
    file in the image tarball as it is ingested into docker.LoadImage, which
    preserves the metadata. Renaming in this fashion also allows just the
    image id (as assigned by the image service) to be used without a prefix,
    so helper methods around prefixing have been removed.

--- image.go
+++ image.go
@@ -1,31 +1,17 @@
 package mdocker
 
 import (
+	"archive/tar"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
-	"strings"
+	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/fsouza/go-dockerclient"
 	"github.com/mistifyio/mistify-agent/rpc"
 )
-
-var repoPrefix = "mistify-imports"
-
-func repoNameFromID(imageID string) string {
-	return fmt.Sprintf("%s/%s", repoPrefix, imageID)
-}
-
-func idFromRepoTag(repoTag string) string {
-	repo, _ := docker.ParseRepositoryTag(repoTag)
-	parts := strings.Split(repo, "/")
-	if len(parts) != 2 {
-		return ""
-	}
-	if parts[0] == repoPrefix {
-		return parts[1]
-	}
-	return ""
-}
 
 // ListImages retrieves a list of Docker images
 func (md *MDocker) ListImages(h *http.Request, request *rpc.ImageRequest, response *rpc.ImageResponse) error {
@@ -37,14 +23,12 @@ func (md *MDocker) ListImages(h *http.Request, request *rpc.ImageRequest, respon
 	}
 	images := make([]*rpc.Image, 0, len(apiImages))
 	for _, ai := range apiImages {
-		id := idFromRepoTag(ai.RepoTags[0])
-		if id != "" {
-			images = append(images, &rpc.Image{
-				Id:   id,
-				Type: "container",
-				Size: uint64(ai.Size) / 1024 / 1024,
-			})
-		}
+		id, _ := docker.ParseRepositoryTag(ai.RepoTags[0])
+		images = append(images, &rpc.Image{
+			Id:   id,
+			Type: "container",
+			Size: uint64(ai.Size) / 1024 / 1024,
+		})
 	}
 
 	response.Images = images
@@ -53,7 +37,7 @@ func (md *MDocker) ListImages(h *http.Request, request *rpc.ImageRequest, respon
 
 // GetImage retrieves information about a specific Docker image
 func (md *MDocker) GetImage(h *http.Request, request *rpc.ImageRequest, response *rpc.ImageResponse) error {
-	image, err := md.client.InspectImage(repoNameFromID(request.Id))
+	image, err := md.client.InspectImage(request.Id)
 	if err != nil {
 		return err
 	}
@@ -68,16 +52,10 @@ func (md *MDocker) GetImage(h *http.Request, request *rpc.ImageRequest, response
 	return nil
 }
 
-// ImportImage downloads a new container image from the image service and
+// LoadImage downloads a new container image from the image service and
 // imports it into Docker
-func (md *MDocker) ImportImage(h *http.Request, request *rpc.ImageRequest, response *rpc.ImageResponse) error {
-	repo := repoNameFromID(request.Id)
-
-	opts := docker.ImportImageOptions{
-		Repository: repo,
-		Source:     fmt.Sprintf("http://%s/images/%s/download", md.imageService, request.Id),
-		Tag:        "latest",
-	}
+func (md *MDocker) LoadImage(h *http.Request, request *rpc.ImageRequest, response *rpc.ImageResponse) error {
+	repo := request.Id
 
 	// Check if we already have the image to avoid unnecessary pulling
 	image, err := md.client.InspectImage(repo)
@@ -86,7 +64,36 @@ func (md *MDocker) ImportImage(h *http.Request, request *rpc.ImageRequest, respo
 	}
 
 	if image == nil {
-		if err := md.client.ImportImage(opts); err != nil {
+		// Docker Import lets the image repo get renamed, but it strips
+		// metadata (which includes the set cmd). Docker Load doesn't let the
+		// repo get renamed and doesn't return the image id or repo after
+		// loading, but does preserve metadata. Since a rename to use the
+		// image-service assigned id and metadata with cmd are both necessary,
+		// the only way forward is to update the repositories file inside and
+		// then load it.
+		source := fmt.Sprintf("http://%s/images/%s/download", md.imageService, request.Id)
+		resp, err := http.Get(source)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return ErrorHTTPCode{
+				Expected: http.StatusOK,
+				Code:     resp.StatusCode,
+				Source:   source,
+			}
+		}
+
+		pipeReader, pipeWriter := io.Pipe()
+
+		go fixRepositoriesFile(repo, resp.Body, pipeWriter)
+
+		opts := docker.LoadImageOptions{
+			InputStream: pipeReader,
+		}
+		if err := md.client.LoadImage(opts); err != nil {
 			return err
 		}
 
@@ -106,9 +113,90 @@ func (md *MDocker) ImportImage(h *http.Request, request *rpc.ImageRequest, respo
 	return nil
 }
 
+func fixRepositoriesFile(newRepoName string, in io.Reader, out io.WriteCloser) {
+	defer out.Close()
+	tarReader := tar.NewReader(in)
+	tarWriter := tar.NewWriter(out)
+	defer tarWriter.Close()
+
+	for {
+		header, err := tarReader.Next()
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			log.WithField("error", err).Error("failed to get next tar header")
+			return
+		}
+
+		switch header.Typeflag {
+		case tar.TypeReg:
+			// Update the repo name tarReader the repositories file
+			if header.Name == "repositories" {
+				// Read the file and parse the JSON
+				inBytes := make([]byte, header.Size)
+				if _, err := tarReader.Read(inBytes); err != nil {
+					log.WithField("error", err).Error("failed to read repositories file")
+					return
+				}
+				origRepo := map[string]interface{}{}
+				if err := json.Unmarshal(inBytes, &origRepo); err != nil {
+					log.WithField("error", err).Error("failed to parse repositories json")
+					return
+				}
+				// Should only be one key. Replace it with the new repo name
+				newRepo := map[string]interface{}{}
+				for oldName := range origRepo {
+					newRepo[newRepoName] = origRepo[oldName]
+					break
+				}
+
+				// Update the header
+				outBytes, err := json.Marshal(newRepo)
+				if err != nil {
+					log.WithField("error", err).Error("failed to marshal repositories json")
+					return
+				}
+				header.Size = int64(len(outBytes))
+				header.ModTime = time.Now()
+
+				// Write the new header and data
+				if err := tarWriter.WriteHeader(header); err != nil {
+					log.WithField("error", err).Error("failed to write repositories header")
+					return
+				}
+				if _, err := tarWriter.Write(outBytes); err != nil {
+					log.WithField("error", err).Error("failed to write repositories json")
+					return
+				}
+			} else {
+				// Direct copy
+				if err := tarWriter.WriteHeader(header); err != nil {
+					log.WithField("error", err).Error("failed to write tar header")
+					return
+				}
+				if _, err := io.Copy(tarWriter, tarReader); err != nil {
+					log.WithField("error", err).Error("failed to copy tar file")
+					return
+				}
+			}
+		default:
+			// Direct copy
+			if err := tarWriter.WriteHeader(header); err != nil {
+				log.WithField("error", err).Error("failed to write tar header")
+				return
+			}
+			if _, err := io.Copy(tarWriter, tarReader); err != nil {
+				log.WithField("error", err).Error("failed to copy tar body")
+				return
+			}
+		}
+	}
+}
+
 // DeleteImage deletes a Docker image
 func (md *MDocker) DeleteImage(h *http.Request, request *rpc.ImageRequest, response *rpc.ImageResponse) error {
-	image, err := md.client.InspectImage(repoNameFromID(request.Id))
+	image, err := md.client.InspectImage(request.Id)
 	if err != nil {
 		return err
 	}
